f9880d2aed4c3ae8895a4969fea23d5a145eb9b1
Stacker <ultragaming1993@gmail.com>
2018-04-07 01:09:15 +0200

    Settings form functionality
    it's a begin..

--- Client/Forms/FrmSettings.cs
+++ Client/Forms/FrmSettings.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
 
 namespace BotFarm
 {
@@ -7,6 +9,25 @@ namespace BotFarm
         public FrmSettings()
         {
             InitializeComponent();
+
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            cbAfk.Checked = Convert.ToBoolean(config.AppSettings.Settings["AFKcheck"].Value);
+            cboxAfkStatus.SelectedIndex = Convert.ToInt32(config.AppSettings.Settings["AFKstatus"].Value);
+            numAfkMins.Value = Convert.ToDecimal(config.AppSettings.Settings["AFKmins"].Value);
+            config.AppSettings.Settings.Add("AFKstatus", cboxAfkStatus.SelectedIndex.ToString());
+            config.Save(ConfigurationSaveMode.Modified);
+        }
+
+        private void btnSave_Click(object sender, System.EventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+            config.AppSettings.Settings.Remove("AFKcheck");
+            config.AppSettings.Settings.Remove("AFKstatus");
+            config.AppSettings.Settings.Remove("AFKmins");
+            config.AppSettings.Settings.Add("AFKcheck", cbAfk.Checked.ToString());
+            config.AppSettings.Settings.Add("AFKstatus", cboxAfkStatus.SelectedIndex.ToString());
+            config.AppSettings.Settings.Add("AFKmins", numAfkMins.Value.ToString());
+            config.Save(ConfigurationSaveMode.Modified);
         }
     }
 }
--- Client/Properties/Settings.Designer.cs
+++ Client/Properties/Settings.Designer.cs
@@ -8,19 +8,14 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System.CodeDom.Compiler;
-using System.Configuration;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
-
 namespace Client.Properties {
     
     
-    [CompilerGenerated()]
-    [GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
-    internal sealed partial class Settings : ApplicationSettingsBase {
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
-        private static Settings defaultInstance = ((Settings)(Synchronized(new Settings())));
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
         public static Settings Default {
             get {
@@ -28,9 +23,9 @@ namespace Client.Properties {
             }
         }
         
-        [UserScopedSetting()]
-        [DebuggerNonUserCode()]
-        [DefaultSettingValue("Bop")]
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("stacker")]
         public string Username {
             get {
                 return ((string)(this["Username"]));
@@ -40,9 +35,9 @@ namespace Client.Properties {
             }
         }
         
-        [UserScopedSetting()]
-        [DebuggerNonUserCode()]
-        [DefaultSettingValue("Ben")]
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("lol123")]
         public string Password {
             get {
                 return ((string)(this["Password"]));
