e5dbe0960be699856b4e0d2b54497de49e26d6d2
Glenn van Ewijk <glennvanewijk@gmail.com>
2018-07-07 14:24:05 +0200

    Implemented checks regarding command-only input crashing the client (#26)

--- Client/Forms/FrmChat.cs
+++ Client/Forms/FrmChat.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Client;
 using Client.Forms;
 using Client.Properties;
+using Client.Util;
 
 
 namespace BotFarm
@@ -382,77 +383,97 @@ namespace BotFarm
             // Redefine last chat message timestamp as current datetime for AFK detection regardless of message succession.
             lastChatMessage = DateTime.Now;
 
-            // Store actual user-typed value in local variable for further processing.
-            var lMessage = textMessage.Text;
-
-            // Define string-array for holding the actual message components.
-            var lValueParts = lMessage.Split(new[] { ' ' }, 2);
-
-            if (lMessage.StartsWith("/"))
+            try
             {
-                // Assume that an entered value consists out of multiple parts and assess first index of parts prior to handling actual content.
-                var lCommand = lValueParts[0];
-                switch (lCommand)
-                {
-                    default:
-                    case "/s":
-                    case "/":
-                        _currentChannel = Channel.Say;
-                        SessionInit.Instance.factoryGame.DoSayChat(lValueParts[1]);
-                        break;
-                    case "/w":
-                        lValueParts = lValueParts[1].Split(new[] {' '}, 2);
-                        _currentChannel = Channel.Whisper;
-                        _currentChannelUser = lValueParts[0];
-                        SessionInit.Instance.factoryGame.DoWhisperChat(lValueParts[1], lValueParts[0]);
-                        break;
-                    case "/g":
-                        _currentChannel = Channel.Guild;
-                        SessionInit.Instance.factoryGame.DoGuildChat(lValueParts[1]);
-                        break;
-                    case "/in":
-                        SessionInit.Instance.factoryGame.InvitePlayerToParty(lValueParts[1]);
-                        break;
-                    case "/p":
-                        _currentChannel = Channel.Party;
-                        SessionInit.Instance.factoryGame.DoPartyChat(lValueParts[1]);
-                        break;
-                    case "/1":
-                    case "/2":
-                    case "/3":
-                    case "/4":
-                        _currentChannel = (Channel)int.Parse(lValueParts[0].Replace("/", ""));
-                        SessionInit.Instance.factoryGame.SayChannel(lValueParts[1],
-                            int.Parse(lValueParts[0].Replace("/", "")));
-                        break;
-                }
+                // Store actual user-typed value in local variable for further processing.
+                var lMessage = textMessage.Text;
 
-                lblChannelIndicator.Text = $"{_currentChannel.ToString()}:";
+                // Define string-array for holding the actual message components.
+                var lValueParts = lMessage.Split(new[] { ' ' }, 2);
+
+                if (lMessage.StartsWith("/"))
+                {
+                    // Assume that an entered value consists out of multiple parts and assess first index of parts prior to handling actual content.
+                    var lCommand = lValueParts[0];
+                    switch (lCommand)
+                    {
+                        default:
+                        case "/s":
+                        case "/":
+                            if (lValueParts.Length < 2) throw new CommandSyntaxException("Say requires at least one parameter (/(s) [message])");
+
+                            _currentChannel = Channel.Say;
+                            SessionInit.Instance.factoryGame.DoSayChat(lValueParts[1]);
+                            break;
+                        case "/w":
+                            if (lValueParts.Length < 2 || lValueParts[1].Split(new[] { ' ' }, 2).Length < 2)
+                                throw new CommandSyntaxException("Whispering requires three parameters (/w [user] [message])");
+
+                            lValueParts = lValueParts[1].Split(new[] {' '}, 2);
+                            _currentChannel = Channel.Whisper;
+                            _currentChannelUser = lValueParts[0];
+                            SessionInit.Instance.factoryGame.DoWhisperChat(lValueParts[1], _currentChannelUser);
+                            break;
+                        case "/g":
+                            if (lValueParts.Length < 2) throw new CommandSyntaxException("Guild chat requires one parameter (/g [message])");
+
+                            _currentChannel = Channel.Guild;
+                            SessionInit.Instance.factoryGame.DoGuildChat(lValueParts[1]);
+                            break;
+                        case "/in":
+                            if (lValueParts.Length < 2) throw new CommandSyntaxException("Invitation requires one parameter (/in [user])");
+
+                            SessionInit.Instance.factoryGame.InvitePlayerToParty(lValueParts[1]);
+                            break;
+                        case "/p":
+                            if (lValueParts.Length < 2) throw new CommandSyntaxException("Party chat requires one parameter (/p [message])");
+
+                            _currentChannel = Channel.Party;
+                            SessionInit.Instance.factoryGame.DoPartyChat(lValueParts[1]);
+                            break;
+                        case "/1":
+                        case "/2":
+                        case "/3":
+                        case "/4":
+                            if (lValueParts.Length < 2) throw new CommandSyntaxException("Channel messages expect at least one parameter (/1-4 [message])");
+
+                            _currentChannel = (Channel)int.Parse(lValueParts[0].Replace("/", ""));
+                            SessionInit.Instance.factoryGame.SayChannel(lValueParts[1],
+                                int.Parse(lValueParts[0].Replace("/", "")));
+                            break;
+                    }
+
+                    lblChannelIndicator.Text = $"{_currentChannel.ToString()}:";
+                }
+                else
+                {
+                    switch (_currentChannel)
+                    {
+                        default:
+                        case Channel.Say:
+                            SessionInit.Instance.factoryGame.DoSayChat(lMessage);
+                            break;
+                        case Channel.Whisper:
+                            SessionInit.Instance.factoryGame.DoWhisperChat(lMessage, _currentChannelUser);
+                            break;
+                        case Channel.Guild:
+                            SessionInit.Instance.factoryGame.DoGuildChat(lMessage);
+                            break;
+                        case Channel.Party:
+                            SessionInit.Instance.factoryGame.DoPartyChat(lMessage);
+                            break;
+                        case Channel.Channel1:
+                        case Channel.Channel2:
+                        case Channel.Channel3:
+                        case Channel.Channel4:
+                            SessionInit.Instance.factoryGame.SayChannel(lMessage, (int)_currentChannel);
+                            break;
+                    }
+                }
             }
-            else
+            catch (CommandSyntaxException lEx)
             {
-                switch (_currentChannel)
-                {
-                    default:
-                    case Channel.Say:
-                        SessionInit.Instance.factoryGame.DoSayChat(lMessage);
-                        break;
-                    case Channel.Whisper:
-                        SessionInit.Instance.factoryGame.DoWhisperChat(lMessage, _currentChannelUser);
-                        break;
-                    case Channel.Guild:
-                        SessionInit.Instance.factoryGame.DoGuildChat(lMessage);
-                        break;
-                    case Channel.Party:
-                        SessionInit.Instance.factoryGame.DoPartyChat(lMessage);
-                        break;
-                    case Channel.Channel1:
-                    case Channel.Channel2:
-                    case Channel.Channel3:
-                    case Channel.Channel4:
-                        SessionInit.Instance.factoryGame.SayChannel(lMessage, (int)_currentChannel);
-                        break;
-                }
+                AppendText(ChatWindow, $"[System] : {lEx.Message}.\r\n", Color.DarkRed, true);
             }
 
             // Clear textMessage TextBox control to prevent 'easy' spamming.
